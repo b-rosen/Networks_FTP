8bd71c53cc0b72969b0e42aa7e05152456b1eeed
Shane <shanehouse2705@gmail.com>
2018-03-19 12:16:35 +0200

    GUI work

--- Client/clientGUI.py
+++ Client/clientGUI.py
@@ -1,9 +1,15 @@
-import Tkinter, tkFileDialog
+from Tkinter import *
+from PIL import Image, ImageTk
 
 clientWindow = Tk()
 clientWindow.title("FTP Client")
 clientWindow.geometry('640x360')
-connectButton = Button(clientWindow,text="Connect")
-connectButton.grid(column=2,row=2)
-file = tkFileDialog.askopenfilename()
+openFImg = Image.open("./images/openfile.png")
+openFTkImg = ImageTk.PhotoImage(openFImg)
+openFileButton = Button(clientWindow,image=openFTkImg,height=20,width=20)
+downloadButton = Button(clientWindow,image="")
+connectButton = Button(clientWindow,image="")
+openFileButton.grid(column=1,row=1)
+downloadButton.grid(column=2,row=1)
+connectButton.grid(column=3,row=1)
 clientWindow.mainloop()
