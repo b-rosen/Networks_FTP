05cd75d110c5379c4c7642a329a0a10b98fc0b90
Shane <shanehouse2705@gmail.com>
2018-03-21 13:18:15 +0200

    added user select

--- Client/clientGUI.py
+++ Client/clientGUI.py
@@ -14,7 +14,7 @@ class clientGUI(Tk):
         self.configure(background="#12d168")
         
         self.pages = {}
-        for p in (serverSelectPage,logInPage,mainPage):
+        for p in (serverSelectPage,guestOrUserPage,logInPage,mainPage):
             page = p(pageHolder,self)
             self.pages[p] = page
             page.grid(row=0, column=0, sticky="NESW")
@@ -31,33 +31,53 @@ class serverSelectPage(Frame):
         Frame.__init__(self,parent)
         self.configure(background="#12d168")
                        
-        serverLabel = Label(self, text="Server Name:",font=("Times New Roman",12),background="#12d168")
+        serverLabel = Label(self, text="Server Name:",font=("Times New Roman",12),background="#12d168",fg="#4d12b5")
         serverLabel.place(x=320,y=150,anchor=CENTER)
         
-        serverName = Entry(self, width=30)
+        serverName = Entry(self, width=30,fg="#4d12b5")
         serverName.place(x=320,y=170,anchor=CENTER)
         
-        connectButton = Button(self, text="connect to server",font=("Times New Roman",12),background="#12d168",command=lambda: gui.display(logInPage))
+        connectButton = Button(self, text="connect to server",font=("Times New Roman",12),background="#12d168",fg="#4d12b5",command=lambda: gui.display(guestOrUserPage))
         connectButton.place(x=320,y=210,anchor=CENTER)
+        
+class guestOrUserPage(Frame):
+    def __init__(self,parent,gui):
+        Frame.__init__(self,parent)
+        self.configure(background="#12d168")
+        selected = IntVar()
+             
+        userSelect = Radiobutton(self,text="user",font=("Times New Roman",12),background="#12d168",fg="#4d12b5",value=1,variable=selected)
+        userSelect.place(x=320,y=150,anchor=CENTER)
+        
+        guestSelect = Radiobutton(self,text="guest",font=("Times New Roman",12),background="#12d168",fg="#4d12b5",value=2,variable=selected)
+        guestSelect.place(x=320,y=180,anchor=CENTER)
+        
+        def getSelected():
+            if selected.get() == 1:
+                gui.display(logInPage)
+            else:
+                gui.display(mainPage)
+        continueButton = Button(self, text="Continue",background="#12d168",fg="#4d12b5",font=("Times New Roman",12),command=getSelected)
+        continueButton.place(x=320,y=220,anchor=CENTER)
         
 class logInPage(Frame):
     def __init__(self,parent,gui):
         Frame.__init__(self,parent)
         self.configure(background="#12d168")
         
-        userLabel = Label(self, text="User Name:",background="#12d168",font=("Times New Roman",12))
+        userLabel = Label(self, text="User Name:",background="#12d168",fg="#4d12b5",font=("Times New Roman",12))
         userLabel.place(x=320,y=115,anchor=CENTER)
         
-        userName = Entry(self, width=30)
+        userName = Entry(self, width=30,fg="#4d12b5")
         userName.place(x=320,y=135,anchor=CENTER)
         
-        passwordLabel = Label(self, text="Password:",background="#12d168",font=("Times New Roman",12))
+        passwordLabel = Label(self, text="Password:",background="#12d168",fg="#4d12b5",font=("Times New Roman",12))
         passwordLabel.place(x=320,y=165,anchor=CENTER)
         
-        password = Entry(self, width=30)
+        password = Entry(self, width=30,fg="#4d12b5")
         password.place(x=320,y=185,anchor=CENTER)
         
-        logInButton = Button(self, text="Log In",background="#12d168",font=("Times New Roman",12),command=lambda: gui.display(mainPage))
+        logInButton = Button(self, text="Log In",background="#12d168",fg="#4d12b5",font=("Times New Roman",12),command=lambda: gui.display(mainPage))
         logInButton.place(x=320,y=225,anchor=CENTER)
         
 class mainPage(Frame):
@@ -65,16 +85,16 @@ class mainPage(Frame):
         Frame.__init__(self,parent)
         self.configure(background="#12d168")
         
-        openFileButton = Button(self,text="open",background="#12d168",height=1,width=7)
+        openFileButton = Button(self,text="open",background="#12d168",fg="#4d12b5",height=1,width=7)
         openFileButton.place(x=260,y=85,anchor=CENTER)
         
-        upFileButton = Button(self,text="up",background="#12d168",height=1,width=7)
+        upFileButton = Button(self,text="up",background="#12d168",fg="#4d12b5",height=1,width=7)
         upFileButton.place(x=320,y=85,anchor=CENTER)
         
-        downloadButton = Button(self,text="download",background="#12d168",height=1,width=7)
+        downloadButton = Button(self,text="download",background="#12d168",fg="#4d12b5",height=1,width=7)
         downloadButton.place(x=380,y=85,anchor=CENTER)
         
-        serverList = Listbox(self, height=10,width=30)
+        serverList = Listbox(self, height=10,width=30,fg="#4d12b5")
         serverList.place(x=320,y=180,anchor=CENTER)
         
         
