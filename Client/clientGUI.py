7cf68097162e9f6f887a277907a9691804ce9c4a
Benjy Rosen <858324@students.wits.ac.za>
2018-04-02 21:28:31 +0200

    Small fix to output of gui when downloading file

--- Client/clientGUI.py
+++ Client/clientGUI.py
@@ -6,7 +6,7 @@ import Client_FTP
 
 ''' All Code in this file is Written by Shane House (749524) '''
 
-#this class is the base of the GUI that holds the window 
+#this class is the base of the GUI that holds the window
 class clientGUI(Tk):
     def __init__(self,*args,**kwargs):
         Tk.__init__(self,*args,**kwargs)
@@ -15,14 +15,14 @@ class clientGUI(Tk):
         pageHolder.pack(side="top", fill="both", expand=True)
         pageHolder.grid_rowconfigure(0, weight=1)
         pageHolder.grid_columnconfigure(0, weight=1)
-        
+
         #Names the window and sets its size
         self.title("Client FTP")
         self.geometry("640x360")
         self.configure(background="#12d168")
-        
+
         #Creates an array of the diferent fames that will be shown in the main
-        #window            
+        #window
         self.pages = {}
         for p in (serverSelectPage,guestOrUserPage,logInPage,mainPage):
             page = p(pageHolder,self)
@@ -30,12 +30,12 @@ class clientGUI(Tk):
             page.grid(row=0, column=0, sticky="NESW")
 
         self.display(serverSelectPage)
-        
+
     #A function the bring the page passed to it to the front
     def display(self, pageToDisp):
         page = self.pages[pageToDisp]
         page.tkraise()
-        
+
     #Returns the class of the same name stored in the frame array
     def getPage(self, pageName):
         return self.pages[pageName]
@@ -46,11 +46,11 @@ class serverSelectPage(Frame):
         Frame.__init__(self,parent)
         self.configure(background="#12d168")
         #Labels are the text elements that are shown on screen
-        #The place function determines where on the screen the 
+        #The place function determines where on the screen the
         #element will be placed
         serverLabel = Label(self, text="Server Name:",font=("Times New Roman",12),background="#12d168",fg="#4d12b5")
         serverLabel.place(x=320,y=115,anchor=CENTER)
-        
+
         #Entries are fields where the user can input data to be used.
         #The entries in this class store the server address and port to connect
         #to
@@ -62,11 +62,11 @@ class serverSelectPage(Frame):
 
         portName = Entry(self, width=30,fg="#4d12b5")
         portName.place(x=320,y=185,anchor=CENTER)
-        
+
         #Runs the functions if the user presses the enter key
         serverName.bind('<Return>', lambda _: serverInput())
         portName.bind('<Return>', lambda _: serverInput())
-        
+
         #This function tries to establish a connection to the server, if successful
         #it makes the main window bring forward the log in page. If it is unsuccessful
         #it displays an error message
@@ -78,7 +78,7 @@ class serverSelectPage(Frame):
                 gui.display(logInPage)
                 return
             tkMessageBox.showerror("Error",message)
-        
+
         #A button that calls the serverInput() function when pressed
         connectButton = Button(self, text="Connect to server",font=("Times New Roman",12),background="#12d168",fg="#4d12b5",command=serverInput)
         connectButton.place(x=320,y=225,anchor=CENTER)
@@ -115,10 +115,10 @@ class logInPage(Frame):
     def __init__(self,parent,gui):
         Frame.__init__(self,parent)
         self.configure(background="#12d168")
-        
+
         userLabel = Label(self, text="User Name:",background="#12d168",fg="#4d12b5",font=("Times New Roman",12))
         userLabel.place(x=320,y=80,anchor=CENTER)
-        
+
         #The entries in this class hold the username password and account information
         #that will be passed to the server if required
         userName = Entry(self, width=30,fg="#4d12b5")
@@ -139,7 +139,7 @@ class logInPage(Frame):
         userName.bind('<Return>', lambda _: logIn())
         passwordEntry.bind('<Return>', lambda _: logIn())
         accountEntry.bind('<Return>', lambda _: logIn())
-        
+
         #This function attempts to log in to the server
         #It als checks if the server supporst the type, mode
         #and structure used by the client application
@@ -168,7 +168,7 @@ class mainPage(Frame):
     def __init__(self,parent,gui):
         Frame.__init__(self,parent)
         self.configure(background="#12d168")
-                       
+
         #This function logs the user out of the server
         def logOutGUI():
             result, message = Client_FTP.Logout()
@@ -176,25 +176,25 @@ class mainPage(Frame):
                 gui.display(serverSelectPage)
                 return
             tkMessageBox.showerror("Error",message)
-            
+
         #This button calls the log out function when pushed
         logOutButton = Button(self,text="log out",background="#12d168",fg="#4d12b5",command=logOutGUI)
         logOutButton.place(x=590,y=30,anchor=CENTER)
-        
+
         #this is a listbox that is used to display the files and folders in the
         #current server directory
         self.serverList = Listbox(self, height=10,width=30,fg="#4d12b5")
         self.serverList.place(x=320,y=180,anchor=CENTER)
-        
+
         #Calls the function that either downloads a file or changes directory
         #on double click
         self.serverList.bind('<Double-Button-1>', self.downloadFileClick)
-        
+
         #A button that when pressed calls the function that displays the current
         #file and folder names in the current server directory
         listButton = Button(self,text="list",background="#12d168",fg="#4d12b5",command=self.listItems)
         listButton.place(x=320,y=80,width=150,anchor=CENTER)
-        
+
         #Bellow are the buttons pressed to change directory, upload and download
         #Files there is extra code here to display images for these buttons.
         #All the images were created by Papirus Development Team and are free
@@ -230,9 +230,9 @@ class mainPage(Frame):
         upFileButton.image = upFileButImg
         upFileText = Label(self, text="Go Up",background="#12d168",fg="#4d12b5",font=("Times New Roman",9))
         upFileText.place(x=70,y=58,anchor=CENTER)
-    
+
     #This function calls the client_FTP functions to download a file from the server
-    #and save it to the downloads folder. It also displays error messages 
+    #and save it to the downloads folder. It also displays error messages
     #if unsuccessful and a message when the operation completes successfully
     def downloadFile(self):
         try:
@@ -253,10 +253,10 @@ class mainPage(Frame):
                 tkMessageBox.showinfo('FTP Client', 'File Downloaded Successfully')
                 return
         tkMessageBox.showerror("Error", msg)
-    
-    #This function will either download a file or go into the directory of 
+
+    #This function will either download a file or go into the directory of
     #whatever is double clicked in the list box it also displays errors if unsucessful
-    #Or nothing is selected. 
+    #Or nothing is selected.
     def downloadFileClick(self, event):
         try:
             fileName = self.serverList.get(self.serverList.curselection()[0])
@@ -271,7 +271,7 @@ class mainPage(Frame):
             if reply:
                 result, msg = Client_FTP.Download(sFilePath,cFilePath)
                 if result:
-                    tkMessageBox.showinfo('FTP Client', 'File Downloaded Successfully' + sFilePath)
+                    tkMessageBox.showinfo('FTP Client', 'File Downloaded Successfully\n' + sFilePath)
                     return
             tkMessageBox.showerror("Error", msg)
             return
@@ -289,9 +289,9 @@ class mainPage(Frame):
                 return
             tkMessageBox.showerror("Error", msg)
 
-    #This function calls the client_FTP functions to upload a file to the server, it displays 
+    #This function calls the client_FTP functions to upload a file to the server, it displays
     #Error messages if unsuccessful and a message to show the operation completed
-    #Successfully 
+    #Successfully
     def uploadFile(self):
         fileToUploadPath = tkFileDialog.askopenfilename()
         if fileToUploadPath == '':
@@ -306,7 +306,7 @@ class mainPage(Frame):
                 tkMessageBox.showinfo('FTP Client', 'File uploaded Successfully')
                 return
         tkMessageBox.showerror("Error", msg)
-        
+
     #This function calls the function in client_FTP to change directories to the one highlighted
     #in the list box. It displays errors if unsuccessful or nothing is selected
     def openFolder(self):
@@ -331,7 +331,7 @@ class mainPage(Frame):
             tkMessageBox.showinfo('FTP Client', 'Folder changed Successfully')
             return
         tkMessageBox.showerror("Error", msg)
-    
+
     #Calls the function in the client_FTP to change up a directory
     def goUp(self):
         reply, msg = Client_FTP.ChangeUp()
@@ -341,7 +341,7 @@ class mainPage(Frame):
             return
         tkMessageBox.showerror("Error", msg)
 
-    #Gets a list of the files in the current directory of the server and displays 
+    #Gets a list of the files in the current directory of the server and displays
     #them in the listbox
     def listItems(self):
         reply, msg = Client_FTP.PassiveMode()
